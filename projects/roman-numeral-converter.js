526e9235648bc3c628f83402b68638ed875297e9
ild0tt0re <ing.annunziata@gmail.com>
2020-10-19 19:51:22 +0200

    add Roman Numeral Converter project

--- projects/roman-numeral-converter.js
+++ projects/roman-numeral-converter.js
@@ -1,45 +1,30 @@
 /** JavaScript Algorithms and Data Structures Projects: Roman Numeral Converter */
 
-/*
-1. [] check valid number 
-2. [] split number by digits
-3. [] 
-*/
-
-var romanSymbols = [
-    {value: 1, symbol: 'I'},
-    {value: 5, symbol: 'V'},
-    {value: 10, symbol: 'X'},
-    {value: 50, symbol: 'L'},
-    {value: 100, symbol: 'C'},
-    {value: 500, symbol: 'D'},
-    {value: 1000, symbol: 'M'}
-];
-
-
-function splitNumberByDigitsAndMultiply(n) {
-    var a1 = String(n).split('').reverse();
-    var a2 = a1.map((digit, i) => {
-        var multiplierByDigit = 10**i;
-        if (multiplierByDigit > 0){
-            return digit * multiplierByDigit;
-        }
-
-        return digit;
-    });
-
-    return a2.reverse();
+const romanSymbolsValue = {
+  M: 1000,
+  CM: 900,
+  D: 500,
+  CD: 400,
+  C: 100,
+  XC: 90,
+  L: 50,
+  XL: 40,
+  X: 10,
+  IX: 9,
+  V: 5,
+  IV: 4,
+  I: 1,
 }
 
-function convertDigitToSymbol(n) {
-
+function convertToRoman(num) {
+  let romanOutput = ''
+  for (let romanSymbol in romanSymbolsValue) {
+    while (num >= romanSymbolsValue[romanSymbol]) {
+      romanOutput += romanSymbol
+      num -= romanSymbolsValue[romanSymbol]
+    }
+  }
+  return romanOutput
 }
 
-function main(n) {
-    var a = splitNumberByDigitsAndMultiply(n);
-    console.log(a)
-    convertDigitToSymbol(a);
-
-}
-
-main(547);
+convertToRoman(36)
